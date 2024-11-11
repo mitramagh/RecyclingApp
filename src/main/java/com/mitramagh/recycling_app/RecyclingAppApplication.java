93b5a6b7da47125450e92ad14cdc1f0b59efa896
Mitra <mit.magh@gmail.com>
2024-11-11 08:46:55 -0800

    Project Setup + DATABASE MySQL

---
+++ src/main/java/com/mitramagh/recycling_app/RecyclingAppApplication.java
@@ -0,0 +1,13 @@
+package com.mitramagh.recycling_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyclingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyclingAppApplication.class, args);
+	}
+
+}
